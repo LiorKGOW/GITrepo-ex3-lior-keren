6e490a61f950229ca07a4444b1c64da21b943df5
Lior Keren <liorkgow@gmail.com>
2024-05-09 01:13:11 +0300

    Change in print of LiorKeren

--- LiorKeren.java
+++ LiorKeren.java
@@ -1,7 +1,7 @@
 public class LiorKeren {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.print("Lior Keren");
+            System.out.println("Lior Keren, ID: 208547992");
         }
     }
 }
